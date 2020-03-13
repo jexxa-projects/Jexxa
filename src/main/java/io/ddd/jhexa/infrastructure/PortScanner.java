847369dae9109dd2582dc9aa85eed3bb99f4018a
repplix <michael.repplinger@icloud.com>
2020-03-13 17:51:05 +0100

    * correted dependencies

--- src/main/java/io/ddd/jhexa/infrastructure/PortScanner.java
+++ src/main/java/io/ddd/jhexa/infrastructure/PortScanner.java
@@ -1,6 +1,7 @@
 package io.ddd.jhexa.infrastructure;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import io.github.classgraph.ClassGraph;
@@ -37,6 +38,30 @@ public class PortScanner
     {
         this.packageName = packageName;
     }
+
+   /* public Object createCorrespondingObject(Method method)
+    {
+        Class<?> neededClass = method.getDeclaringClass();
+        for (Class<?> clazz : factories)
+        {
+            for (Method erzeuge : clazz.getMethods())
+            {
+                if (erzeuge.getReturnType() == neededClass)
+                {
+                    try
+                    {
+                        Object factoryObject = clazz.getConstructors()[0].newInstance(PropertiesLoader.getInstance().getProperties());
+                        return erzeuge.invoke(factoryObject);
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return new Exception("Objekt konnte nicht erzeugt werden");
+    }*/
 
     public PortScanner()
     {
