bcdd8d8ebdef8ff9c43d464c46c21f1c75384e1e
repplix <michael.repplinger@icloud.com>
2020-04-11 07:20:26 +0200

    * improved comments for better javadoc

--- src/main/java/io/ddd/jexxa/core/factory/PortFactory.java
+++ src/main/java/io/ddd/jexxa/core/factory/PortFactory.java
@@ -13,19 +13,21 @@ import java.util.Properties;
 import io.ddd.jexxa.utils.JexxaLogger;
 import org.apache.commons.lang.Validate;
 
-/*+*
+/**
  * This class is responsible for creating instances of ports or a wrapper for a port including all required parameter.
- *
+ * <p>
  * Conventions for ports
- * 1. A port has a single public constructor with parameters of all required driven adapters.
- *    Note: The need for multiple constructors with different parameters is strong indication of violation of SRP
+ * <ol>
+ *    <li>A port has a single public constructor with parameters of all required driven adapters. <br>
+ *        <strong>Note: The need for multiple constructors with different parameters is strong indication of violation of SRP</strong></li>
  *
- * 2. All parameters of the single constructor of a port must be interfaces.
- *    Note: In case you have to pass concrete instance into the application is a strong indication that separation of technology
- *          stacks from application core is violated
+ *  <li> All parameters of the single constructor of a port must be interfaces.
+ *      <strong>Note: In case you have to pass concrete instance into the application is a strong indication that separation of technology
+ *           stacks from application core is violated.</strong></li>
  *
- * 3. A wrapper for a port has a single public constructor with a single parameter which is a port. In order to create the port properly, the above
- *    conventions must be fulfilled.
+ *  <li>A wrapper for a port has a single public constructor with a single parameter which is a port. In order to create the port properly, the above
+ *      conventions must be fulfilled.</li>
+ * </ol>
  */
 public class PortFactory
 {
@@ -45,8 +47,11 @@ public class PortFactory
         return this;
     }
 
-    /***
+    /**
      * Check if an inbound port including all required driven adapter can be created
+     *
+     * @param inboundPort type of the port to check if it is in general available
+     * @return true in case a constructor fulfilling conventions is available, otherwise false 
      */
     public boolean isAvailable(Class<?> inboundPort)
     {
@@ -54,42 +59,44 @@ public class PortFactory
     }
 
 
-    /***
+    /**
      * Creates a new instance of given inbound port each time this method is called 
      *
      * @see #getInstanceOf(Class, Properties) 
      * @param inboundPort type of the inbound port
-     * @param drivenAdapterProperties properties required to create and configure driven adapter
+     * @param adapterProperties properties required to create and configure driven adapter
+     * @param <T> Type of the object that should be created
      * @return a new instance of inbound port 
      */
-    public <T> T newInstanceOf(Class<T> inboundPort, Properties drivenAdapterProperties)
+    public <T> T newInstanceOf(Class<T> inboundPort, Properties adapterProperties)
     {
         Validate.notNull(inboundPort);
-        Validate.notNull(drivenAdapterProperties);
+        Validate.notNull(adapterProperties);
 
         var supportedConstructor = findConstructor(inboundPort).
                 orElseThrow(() -> new MissingAdapterException(inboundPort, adapterFactory));
 
-        var drivenAdapter = createAdapter(supportedConstructor, drivenAdapterProperties);
+        var drivenAdapter = createAdapter(supportedConstructor, adapterProperties);
         
         return ClassFactory.
                 newInstanceOf(inboundPort, drivenAdapter).
                 orElseThrow();
     }
 
-    /***
+    /**
      * Checks if an instance of given inbound port was already created using this methods and returns this instance.
      * Only if no instance was created so far using this method a new one is created.
      *
      * @see #newInstanceOf(Class, Properties) 
      * @param inboundPort type of the inbound port
-     * @param drivenAdapterProperties properties required to create and configure driven adapter
+     * @param adapterProperties properties required to create and configure driven adapter
+     * @param <T> Type of the object that should be created
      * @return Either a new instance of inbound port or an instance that was previously created 
      */
-    public <T> T getInstanceOf(Class<T> inboundPort, Properties drivenAdapterProperties)
+    public <T> T getInstanceOf(Class<T> inboundPort, Properties adapterProperties)
     {
         Validate.notNull(inboundPort);
-        Validate.notNull(drivenAdapterProperties);
+        Validate.notNull(adapterProperties);
 
 
         var existingInstance = objectPool.getInstance(inboundPort);
@@ -98,12 +105,12 @@ public class PortFactory
             return existingInstance.get();
         }
 
-        var newInstance = newInstanceOf(inboundPort, drivenAdapterProperties);
+        var newInstance = newInstanceOf(inboundPort, adapterProperties);
         objectPool.add(newInstance);
         return newInstance;
     }
 
-    public List<Object> getInstanceOfPorts(Class <? extends Annotation> portAnnotation, Properties drivenAdapterProperties) {
+    public List<Object> getInstanceOfPorts(Class <? extends Annotation> portAnnotation, Properties adapterProperties) {
         var annotationScanner = new DependencyScanner().
                 whiteListPackages(whiteListPackages);
 
@@ -114,7 +121,7 @@ public class PortFactory
 
         scannedInboundPorts.
                 forEach(exceptionCollector(
-                        element -> result.add(getInstanceOf(element, drivenAdapterProperties)),
+                        element -> result.add(getInstanceOf(element, adapterProperties)),
                         exceptionList)
                 );
 
@@ -133,7 +140,7 @@ public class PortFactory
 
 
 
-    /***
+    /**
      * Find a constructor whose parameter can be instantiated
      */
     private Optional<Constructor<?>> findConstructor(Class<?> inboundPort)
@@ -153,7 +160,7 @@ public class PortFactory
     }
 
 
-    private Object[] createAdapter(Constructor<?> portConstructor, Properties drivenAdapterProperties)
+    private Object[] createAdapter(Constructor<?> portConstructor, Properties adapterProperties)
     {
         var objectList = new ArrayList<>();
 
@@ -161,7 +168,7 @@ public class PortFactory
         {
             try
             {
-                objectList.add( adapterFactory.newInstanceOf(portConstructor.getParameterTypes()[i], drivenAdapterProperties) );
+                objectList.add( adapterFactory.newInstanceOf(portConstructor.getParameterTypes()[i], adapterProperties) );
             }
             catch ( Exception e)
             {
@@ -173,7 +180,7 @@ public class PortFactory
         return objectList.toArray();
     }
 
-    /***
+    /**
      * Returns the port that is required by given port wrapper
      */
     private <T> Class<?> getPort(Class<T> portWrapper)
