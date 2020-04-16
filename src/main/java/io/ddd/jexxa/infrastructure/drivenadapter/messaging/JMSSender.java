e9b55b1f65d71fd6e41bf7e9608310444e72e24a
repplix <michael.repplinger@icloud.com>
2020-04-16 05:17:50 +0200

    * added first version of JMSSender

---
+++ src/main/java/io/ddd/jexxa/infrastructure/drivenadapter/messaging/JMSSender.java
@@ -0,0 +1,119 @@
+package io.ddd.jexxa.infrastructure.drivenadapter.messaging;
+
+
+import java.util.Map;
+import java.util.Properties;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ */
+public class JMSSender
+{
+    public static final String JNDI_PROVIDER_URL_KEY = "java.naming.provider.url";
+    public static final String JNDI_USER_KEY = "java.naming.user";
+    public static final String JNDI_PASSWORD_KEY = "java.naming.password";
+    public static final String JNDI_FACTORY_KEY = "java.naming.factory.initial";
+
+
+    public static final String DEFAULT_JNDI_PROVIDER_URL = "tcp://localhost:61616";
+    public static final String DEFAULT_JNDI_USER = "admin";
+    public static final String DEFAULT_JNDI_PASSWORD = "admin";
+    public static final String DEFAULT_JNDI_FACTORY = "org.apache.activemq.jndi.ActiveMQInitialContextFactory";
+
+    private final Properties properties;
+
+    public JMSSender(final Properties properties)
+    {
+        this.properties = properties;
+    }
+    
+    public void sendToTopic(Object message, final String topicName, final Properties messageProperties)
+    {
+        try(final Connection connection = createConnection();
+            final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+        )
+        {
+            final Topic destination = session.createTopic(topicName);
+            final MessageProducer producer = session.createProducer(destination);
+
+            Gson gson = new Gson();
+            final Message textMessage = session.createTextMessage(gson.toJson(message));
+            //TODO: check if we should add type information 
+            //textMessage.setStringProperty("MessageType", message.getClass().getSimpleName());
+
+
+            if (messageProperties != null)
+            {
+                for (Map.Entry<Object, Object> entry : messageProperties.entrySet())
+                {
+                    textMessage.setStringProperty(entry.getKey().toString(), entry.getValue().toString());
+                }
+            }
+            
+            producer.send(textMessage);
+        }
+        catch (JMSException e)
+        {
+            throw new IllegalStateException("Could not send message ", e.getCause());
+        }
+    }
+
+    public void sendToQueue(final Object message, final String queueName, Properties messageProperties)
+    {
+        try (final Connection connection = createConnection();
+             final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
+
+            final Destination destination = session.createQueue(queueName);
+            final MessageProducer producer = session.createProducer(destination);
+
+
+            final Gson gson = new Gson();
+            final Message textMessage = session.createTextMessage(gson.toJson(message));
+            producer.send(textMessage);
+
+            if (messageProperties != null)
+            {
+                for (Map.Entry<Object, Object> entry : messageProperties.entrySet())
+                {
+                    textMessage.setStringProperty(entry.getKey().toString(), entry.getValue().toString());
+                }
+            }
+        }
+        catch (JMSException e)
+        {
+            throw new IllegalStateException("Exception beim Senden der Message", e);
+        }
+    }
+
+
+    Connection createConnection()
+    {
+        try
+        {
+            final InitialContext initialContext = new InitialContext(properties);
+            final ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
+            return connectionFactory.createConnection(properties.getProperty(JNDI_USER_KEY), properties.getProperty(JNDI_PASSWORD_KEY));
+        }
+        catch (NamingException e)
+        {
+            throw new IllegalStateException("No ConnectionFactory available via : " + properties.get(JNDI_PROVIDER_URL_KEY), e);
+        }
+        catch (JMSException e)
+        {
+            throw new IllegalStateException("Can not connect to " + properties.get(JNDI_PROVIDER_URL_KEY), e);
+        }
+    }
+}
---
+++ src/test/java/io/ddd/jexxa/infrastructure/drivenadapter/messaging/JMSSenderTest.java
@@ -0,0 +1,138 @@
+package io.ddd.jexxa.infrastructure.drivenadapter.messaging;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import io.ddd.jexxa.application.domain.valueobject.JexxaValueObject;
+import io.ddd.jexxa.core.JexxaMain;
+import io.ddd.jexxa.infrastructure.drivingadapter.messaging.JMSAdapter;
+import io.ddd.jexxa.infrastructure.drivingadapter.messaging.JMSAdapterTest;
+import io.ddd.jexxa.infrastructure.drivingadapter.messaging.JMSListener;
+import io.ddd.jexxa.utils.JexxaLogger;
+import org.junit.Test;
+
+public class JMSSenderTest
+{
+
+    @Test (timeout = 1000)
+    public void sentMessageToTopic()
+    {
+        var message = new JexxaValueObject(42);
+        var topicName = "JMSSenderTest";
+        var properties = new Properties();
+        properties.put(JMSSender.JNDI_FACTORY_KEY, JMSSender.DEFAULT_JNDI_FACTORY);
+        properties.put(JMSSender.JNDI_PROVIDER_URL_KEY, JMSSender.DEFAULT_JNDI_PROVIDER_URL);
+        properties.put(JMSSender.JNDI_USER_KEY, JMSSender.DEFAULT_JNDI_USER);
+        properties.put(JMSSender.JNDI_PASSWORD_KEY, JMSSender.DEFAULT_JNDI_PASSWORD);
+
+        var messageListener = new MyTopicListener();
+        JMSSender objectUnderTest = new JMSSender(properties);
+
+        JexxaMain jexxaMain = new JexxaMain("JMSSenderTest", properties);
+        jexxaMain.bindToPort(JMSAdapter.class, messageListener);
+
+        //Act
+        jexxaMain.startDrivingAdapters();
+        objectUnderTest.sendToTopic(message, topicName, null);
+
+        //Assert
+        while (messageListener.getMessages().isEmpty())
+        {
+            Thread.onSpinWait();
+        }
+
+        jexxaMain.stopDrivingAdapters();
+
+    }
+
+
+    @Test (timeout = 1000)
+    public void sentMessageToQueue()
+    {
+        var message = new JexxaValueObject(42);
+        var destination = "JMSSenderTest";
+        var properties = new Properties();
+        properties.put(JMSSender.JNDI_FACTORY_KEY, JMSSender.DEFAULT_JNDI_FACTORY);
+        properties.put(JMSSender.JNDI_PROVIDER_URL_KEY, JMSSender.DEFAULT_JNDI_PROVIDER_URL);
+        properties.put(JMSSender.JNDI_USER_KEY, JMSSender.DEFAULT_JNDI_USER);
+        properties.put(JMSSender.JNDI_PASSWORD_KEY, JMSSender.DEFAULT_JNDI_PASSWORD);
+
+        var messageListener = new MyQueueListener();
+        JMSSender objectUnderTest = new JMSSender(properties);
+
+        JexxaMain jexxaMain = new JexxaMain("JMSSenderTest", properties);
+        jexxaMain.bindToPort(JMSAdapter.class, messageListener);
+
+        //Act
+        jexxaMain.startDrivingAdapters();
+        objectUnderTest.sendToQueue(message, destination, null);
+
+        //Assert
+        while (messageListener.getMessages().isEmpty())
+        {
+            Thread.onSpinWait();
+        }
+
+        jexxaMain.stopDrivingAdapters();
+
+    }
+
+
+    static public class MyTopicListener implements MessageListener
+    {
+
+        final List<Message> messageList = new ArrayList<>();
+
+        @Override
+        @JMSListener(destination = "JMSSenderTest", messagingType = JMSListener.MessagingType.TOPIC)
+        public void onMessage(Message message)
+        {
+            try
+            {
+                JexxaLogger.getLogger(JMSAdapterTest.MyListener.class).info(((TextMessage) message).getText());
+                messageList.add(message);
+            }
+            catch ( Exception e) {
+                JexxaLogger.getLogger(JMSAdapterTest.MyListener.class).error(e.getMessage());
+            }
+        }
+
+        public List<Message> getMessages()
+        {
+            return messageList;
+        }
+    }
+
+    static public class MyQueueListener implements MessageListener
+    {
+
+        final List<Message> messageList = new ArrayList<>();
+
+        @Override
+        @JMSListener(destination = "JMSSenderTest", messagingType = JMSListener.MessagingType.QUEUE)
+        public void onMessage(Message message)
+        {
+            try
+            {
+                JexxaLogger.getLogger(JMSAdapterTest.MyListener.class).info(((TextMessage) message).getText());
+                messageList.add(message);
+            }
+            catch ( Exception e) {
+                JexxaLogger.getLogger(JMSAdapterTest.MyListener.class).error(e.getMessage());
+            }
+        }
+
+        public List<Message> getMessages()
+        {
+            return messageList;
+        }
+    }
+
+}
