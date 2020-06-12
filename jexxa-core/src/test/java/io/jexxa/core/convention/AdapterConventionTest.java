32d55b7dc91ac0c6fbe8e318b8eb7a44ff9c08b0
repplix <michael.repplinger@icloud.com>
2020-06-12 15:36:12 +0200

    * added test for conventions for adapter constructor

---
+++ jexxa-core/src/test/java/io/jexxa/core/convention/AdapterConventionTest.java
@@ -0,0 +1,38 @@
+package io.jexxa.core.convention;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.jexxa.application.applicationservice.SimpleApplicationService;
+import io.jexxa.application.infrastructure.drivenadapter.factory.DefaultConstructorAdapter;
+import io.jexxa.application.infrastructure.drivenadapter.factory.FactoryMethodAdapter;
+import io.jexxa.application.infrastructure.drivenadapter.factory.PropertiesConstructorAdapter;
+import io.jexxa.application.infrastructure.drivingadapter.InvalidAdapter;
+import io.jexxa.application.infrastructure.drivingadapter.messaging.SimpleApplicationServiceAdapter;
+import org.junit.jupiter.api.Test;
+
+public class AdapterConventionTest
+{
+    @Test
+    void validateAdapterConvention()
+    {
+        //Assert all Adapter conventions
+        AdapterConvention.validate(DefaultConstructorAdapter.class);
+
+        AdapterConvention.validate(FactoryMethodAdapter.class);
+
+        AdapterConvention.validate(PropertiesConstructorAdapter.class);
+
+        assertThrows(AdapterConventionViolation.class, () -> AdapterConvention.validate(InvalidAdapter.class));
+    }
+
+    @Test
+    void validatePortAdapterConvention()
+    {
+        //Assert all port adapter conventions
+        assertTrue(AdapterConvention.isPortAdapter(SimpleApplicationServiceAdapter.class));
+        assertFalse(AdapterConvention.isPortAdapter(SimpleApplicationService.class));
+    }
+
+}
