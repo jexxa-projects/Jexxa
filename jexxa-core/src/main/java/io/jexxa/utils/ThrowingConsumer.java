5ccc56a2ea515af79215fede8c4723c829094d69
repplix <michael.repplinger@icloud.com>
2020-06-20 06:15:52 +0200

    * changed ThrowingConsumer so that only Exceptions are caught and not Throwable. Note: Throwable would also catch Errors such as OutOfMemeory which is not desired here.

--- jexxa-core/src/main/java/io/jexxa/utils/ThrowingConsumer.java
+++ jexxa-core/src/main/java/io/jexxa/utils/ThrowingConsumer.java
@@ -22,19 +22,19 @@ import java.util.function.Consumer;
  * @param <E> Type of the exception
  */
 @FunctionalInterface
-public interface ThrowingConsumer<T, E extends Throwable> {
+public interface ThrowingConsumer<T, E extends Exception> {
     void accept(T t) throws E;
 
 
     @SuppressWarnings("java:S1181") //Catch Throwable without warning
-    static <T, E extends Throwable> Consumer<T>
+    static <T, E extends Exception> Consumer<T>
     exceptionLogger(ThrowingConsumer<T, E> throwingConsumer) {
         return i -> {
             try
             {
                 throwingConsumer.accept(i);
             }
-            catch (Throwable e)
+            catch (Exception e)
             {
                 JexxaLogger.getLogger(throwingConsumer.getClass()).error(e.getMessage());
             }
@@ -42,13 +42,13 @@ public interface ThrowingConsumer<T, E extends Throwable> {
     }
 
     static <T> Consumer<T>
-    exceptionCollector(ThrowingConsumer<T, Throwable> throwingConsumer, Collection<Throwable> exceptions) {
+    exceptionCollector(ThrowingConsumer<T, Exception> throwingConsumer, Collection<Throwable> exceptions) {
         return i -> {
             try
             {
                 throwingConsumer.accept(i);
             }
-            catch (Throwable e)
+            catch (Exception e)
             {
                 exceptions.add(e);
                 JexxaLogger.getLogger(throwingConsumer.getClass()).error(e.getMessage());
