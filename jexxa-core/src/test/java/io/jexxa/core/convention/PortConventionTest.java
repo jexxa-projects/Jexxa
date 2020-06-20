cec54d5cb61d25772c40fa35e07f24bcd572ccf3
repplix <michael.repplinger@icloud.com>
2020-06-20 10:40:41 +0200

    * added tests

---
+++ jexxa-core/src/test/java/io/jexxa/core/convention/PortConventionTest.java
@@ -0,0 +1,45 @@
+package io.jexxa.core.convention;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Properties;
+
+import io.jexxa.application.applicationservice.InvalidApplicationService;
+import io.jexxa.application.domainservice.IJexxaAggregateRepository;
+import org.junit.jupiter.api.Test;
+
+public class PortConventionTest
+{
+    @Test
+    void invalidPortConsturctor()
+    {
+        //Act/Assert
+        assertThrows(PortConventionViolation.class, () -> PortConvention.validate(InvalidApplicationService.class));
+        assertThrows(PortConventionViolation.class, () -> PortConvention.validate(InvalidApplicationServiceNoInterface.class));
+        assertThrows(PortConventionViolation.class, () -> PortConvention.validate(InvalidApplicationServiceMultipleConstructor.class));
+    }
+
+    @SuppressWarnings("unused")
+    public static class InvalidApplicationServiceMultipleConstructor
+    {
+        public InvalidApplicationServiceMultipleConstructor()
+        {
+
+        }
+
+        public InvalidApplicationServiceMultipleConstructor(IJexxaAggregateRepository jexxaAggregateRepository)
+        {
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static class InvalidApplicationServiceNoInterface
+    {
+        public InvalidApplicationServiceNoInterface(Properties properties)
+        {
+
+        }
+
+    }
+
+}
