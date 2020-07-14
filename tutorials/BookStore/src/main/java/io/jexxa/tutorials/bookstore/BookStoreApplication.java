df7ee3b43ebba3c7a186e5694c9fca8669bd49a3
repplix <michael.repplinger@dillinger.biz>
2020-07-14 16:21:57 +0200

    * added command line parameter for switching between JDBC and IMDB repository stragegy

--- tutorials/BookStore/src/main/java/io/jexxa/tutorials/bookstore/BookStoreApplication.java
+++ tutorials/BookStore/src/main/java/io/jexxa/tutorials/bookstore/BookStoreApplication.java
@@ -2,11 +2,19 @@ package io.jexxa.tutorials.bookstore;
 
 
 import io.jexxa.core.JexxaMain;
+import io.jexxa.infrastructure.drivenadapterstrategy.persistence.IRepository;
 import io.jexxa.infrastructure.drivenadapterstrategy.persistence.RepositoryManager;
 import io.jexxa.infrastructure.drivenadapterstrategy.persistence.imdb.IMDBRepository;
+import io.jexxa.infrastructure.drivenadapterstrategy.persistence.jdbc.JDBCKeyValueRepository;
 import io.jexxa.infrastructure.drivingadapter.jmx.JMXAdapter;
 import io.jexxa.infrastructure.drivingadapter.rest.RESTfulRPCAdapter;
 import io.jexxa.tutorials.bookstore.applicationservice.BookStoreService;
+import io.jexxa.utils.JexxaLogger;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class BookStoreApplication
 {
@@ -16,19 +24,16 @@ public class BookStoreApplication
 
     public static void main(String[] args)
     {
-        RepositoryManager.getInstance().setDefaultStrategy(IMDBRepository.class);
+        RepositoryManager.getInstance().setDefaultStrategy(getDrivenAdapterStrategy(args));
 
         JexxaMain jexxaMain = new JexxaMain(BookStoreApplication.class.getSimpleName());
 
         jexxaMain
                 //Define which outbound ports should be managed by Jexxa
                 .addToApplicationCore(OUTBOUND_PORTS)
-
-                //Define which driven adapter should be used by Jexxa
-                //Note: We can only register one driven adapter for the
                 .addToInfrastructure(DRIVEN_ADAPTER)
 
-                // Bind a REST and JMX adapter to the TimeService
+                // Bind a REST and JMX adapter to the BookStoreService
                 // It allows to access the public methods of the TimeService via RMI over REST or Jconsole
                 .bind(RESTfulRPCAdapter.class).to(BookStoreService.class)
                 .bind(JMXAdapter.class).to(BookStoreService.class)
@@ -41,6 +46,35 @@ public class BookStoreApplication
                 .waitForShutdown()
 
                 .stop();
+    }
+
+
+    private static Class<? extends IRepository> getDrivenAdapterStrategy(String[] args)
+    {
+        Options options = new Options();
+        options.addOption("j", "jdbc", false, "jdbc driven adapter strategy");
+
+        CommandLineParser parser = new DefaultParser();
+        try
+        {
+            CommandLine line = parser.parse( options, args );
+
+            if (line.hasOption("jdbc"))
+            {
+                JexxaLogger.getLogger(BookStoreApplication.class).info("Use persistence strategy: {} ", JDBCKeyValueRepository.class.getSimpleName());
+                return JDBCKeyValueRepository.class;
+            }
+            else
+            {
+                JexxaLogger.getLogger(BookStoreApplication.class).info("Use persistence strategy: {} ", IMDBRepository.class.getSimpleName());
+                return IMDBRepository.class;
+            }
+        }
+        catch( ParseException exp ) {
+            JexxaLogger.getLogger(BookStoreApplication.class)
+                    .error( "Parsing failed.  Reason: {}", exp.getMessage() );
+        }
+        return IMDBRepository.class;
     }
 
 
