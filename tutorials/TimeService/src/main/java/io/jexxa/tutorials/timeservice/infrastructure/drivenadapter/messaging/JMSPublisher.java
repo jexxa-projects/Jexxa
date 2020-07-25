810aa3035c0393c25e8bf9328426513a520645f4
repplix <michael.repplinger@icloud.com>
2020-07-25 07:20:30 +0200

    * updated tutorial to new Jexxa's messaging interface * updated to latest Jexxa release 2.2.0 * updated aspectj

--- tutorials/TimeService/src/main/java/io/jexxa/tutorials/timeservice/infrastructure/drivenadapter/messaging/JMSPublisher.java
+++ tutorials/TimeService/src/main/java/io/jexxa/tutorials/timeservice/infrastructure/drivenadapter/messaging/JMSPublisher.java
@@ -3,7 +3,8 @@ package io.jexxa.tutorials.timeservice.infrastructure.drivenadapter.messaging;
 import java.time.LocalTime;
 import java.util.Properties;
 
-import io.jexxa.infrastructure.drivenadapterstrategy.messaging.JMSSender;
+import io.jexxa.infrastructure.drivenadapterstrategy.messaging.MessageSender;
+import io.jexxa.infrastructure.drivenadapterstrategy.messaging.MessageSenderManager;
 import io.jexxa.tutorials.timeservice.domainservice.ITimePublisher;
 
 @SuppressWarnings("unused")
@@ -11,21 +12,21 @@ public class JMSPublisher implements ITimePublisher
 {
     private static final String TIME_TOPIC = "TimeService";
 
-    private final JMSSender jmsSender;
+    private final MessageSender messageSender;
 
     // For all driven adapter we have to provide either a static factory or a public constructor to
     // enable implicit constructor injection
     public JMSPublisher(Properties properties)
     {
-        this.jmsSender = new JMSSender(properties);
+        this.messageSender = MessageSenderManager.getInstance().getStrategy(properties);
     }
 
     @Override
     public void publish(LocalTime localTime)
     {
         // Send the message to the topic.
-        // Important note: The JMSSender internally uses json to serialize given objects.
-        // Therefore, the receiver must deserialize the received message accordingly
-        jmsSender.sendToTopic(localTime, TIME_TOPIC);
+        messageSender.send(localTime)
+                .toTopic(TIME_TOPIC)
+                .asJson();
     }
 }
