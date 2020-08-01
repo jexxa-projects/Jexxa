7caca26319425b0f04821869e7a7f6f0674adea0
repplix <michael.repplinger@icloud.com>
2020-08-01 15:47:18 +0200

    * removed obsolete suppress warnings

--- jexxa-core/src/main/java/io/jexxa/infrastructure/drivenadapterstrategy/persistence/IRepository.java
+++ jexxa-core/src/main/java/io/jexxa/infrastructure/drivenadapterstrategy/persistence/IRepository.java
@@ -3,13 +3,8 @@ package io.jexxa.infrastructure.drivenadapterstrategy.persistence;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Throws IllegalArgumentException if any operations fails
- */
-@SuppressWarnings("unused")
 public interface IRepository<T, K>
 {
-    @SuppressWarnings("EmptyMethod")
     void update(T aggregate);
 
     void remove(K key);
