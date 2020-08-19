9f16d455afcf0e18f7c937a51edb863dfef5a864
repplix <michael.repplinger@icloud.com>
2020-08-19 05:59:39 +0200

    explicitly create an ssl context which uses manual created trustStore in order to run the test on github

--- jexxa-core/src/test/java/io/jexxa/infrastructure/drivingadapter/rest/HTTPSTest.java
+++ jexxa-core/src/test/java/io/jexxa/infrastructure/drivingadapter/rest/HTTPSTest.java
@@ -3,14 +3,25 @@ package io.jexxa.infrastructure.drivingadapter.rest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.Properties;
+
+import javax.net.ssl.SSLContext;
 
 import io.jexxa.application.applicationservice.SimpleApplicationService;
 import kong.unirest.Unirest;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -25,20 +36,23 @@ class HTTPSTest
     private final SimpleApplicationService simpleApplicationService = new SimpleApplicationService();
 
     @Test
-    void testHTTPSConnection() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException
+    void testHTTPSConnection() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException, CertificateException, IOException
     {
         // NOTE: To run this test we need to create a truststore has described here https://magicmonster.com/kb/prg/java/ssl/pkix_path_building_failed/
         //Arrange -> Nothing to do
         Unirest.shutDown();
 
-       /* SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()
-        {
-            public boolean isTrusted(X509Certificate[] chain, String authType)
-            {
-                return true;
-            }
-        }).build();
-        CloseableHttpClient customHttpClient = HttpClients.custom().setSSLContext(sslContext)
+        //System.out.println(HTTPSTest.class.getResource("trustStore.jks").getFile());
+       // System.out.println(HTTPSTest.class.getResource("/").getFile());
+
+        SSLContext sslContext =  new SSLContextBuilder().loadTrustMaterial(
+                HTTPSTest.class.getResource("/trustStore.jks"), //path to jks file
+                "changeit".toCharArray(), //enters in the truststore password for use
+                new TrustSelfSignedStrategy() //will trust own CA and all self-signed certs
+        ).build();
+
+
+       CloseableHttpClient customHttpClient = HttpClients.custom().setSSLContext(sslContext)
                 .setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
 
         CloseableHttpAsyncClient client = HttpAsyncClients.custom()
@@ -46,11 +60,12 @@ class HTTPSTest
                 .setSSLContext(sslContext).build();
 
         Unirest.config().httpClient(customHttpClient);
-        Unirest.config().asyncClient(client);
-        */
-        Unirest.config().verifySsl(false);
+        Unirest.config().asyncClient(client); 
+
+        /*Unirest.config().verifySsl(false);
         Unirest.config().hostnameVerifier(new NoopHostnameVerifier());
-        
+          */
+
 
         var properties = new Properties();
         var defaultHost = "0.0.0.0";
@@ -63,8 +78,8 @@ class HTTPSTest
         properties.put(RESTfulRPCAdapter.KEYSTORE_PASSWORD, "test123");
         properties.put(RESTfulRPCAdapter.KEYSTORE, "keystore.jks");
 
-        properties.put("javax.net.ssl.trustStore", "truststore");
-        properties.put("javax.net.ssl.trustStorePassword", "changeit");
+        //System.setProperty("javax.net.ssl.trustStore", "trustStore.jks");
+        //System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
 
         var objectUnderTest = new RESTfulRPCAdapter(properties);
         objectUnderTest.register(simpleApplicationService);
