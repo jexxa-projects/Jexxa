f9ae42d823053920b9fc8c2859f8c2ea9f970093
repplix <michael.repplinger@icloud.com>
2020-09-13 09:01:55 +0200

    * added some javadoc

--- jexxa-core/src/main/java/io/jexxa/core/JexxaMain.java
+++ jexxa-core/src/main/java/io/jexxa/core/JexxaMain.java
@@ -18,28 +18,53 @@ import io.jexxa.utils.function.ThrowingConsumer;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 
+/**
+ * JexxaMain is the main entry point for your application to use Jexxa. Within each application only a single instance
+ * of this class must exist.
+ *
+ * In order to control your application (start / shutdown) from within your application and also from outside
+ * JexxaMain provides a so called {@link BoundedContext}.
+ *
+ * To see how to use this class please refer to the tutorials.
+ */
 public class JexxaMain
 {
-
     public static final String JEXXA_APPLICATION_PROPERTIES = "/jexxa-application.properties";
     private static final String JEXXA_CONTEXT_NAME =  "io.jexxa.context.name";
 
     private static final Logger LOGGER = JexxaLogger.getLogger(JexxaMain.class);
 
     private final CompositeDrivingAdapter compositeDrivingAdapter = new CompositeDrivingAdapter();
-    private final Properties properties = new Properties();
-
+    private final Properties properties                 = new Properties();
     private final AdapterFactory drivingAdapterFactory  = new AdapterFactory();
     private final AdapterFactory drivenAdapterFactory   = new AdapterFactory();
     private final PortFactory portFactory               = new PortFactory(drivenAdapterFactory);
 
     private final BoundedContext boundedContext;
 
+    /**
+     * Creates the JexxaMain instance for your application with given context name.
+     * In addition the properties file jexxa-application.properties is load if available in class path.
+     *
+     * Note: When a driving or driven adapter is created, it gets the properties read from properties file.
+     *
+     * @param contextName Name of the BoundedContext. Typically, you should use the name of your application.
+     */
     public JexxaMain(String contextName)
     {
         this(contextName, System.getProperties());
     }
 
+    /**
+     * Creates the JexxaMain instance for your application with given context name.
+     *
+     * Note: The file jexxa-application.properties is loaded if available in class path. Then the
+     * properties are extended by the given properties object. So if you define the same properties in
+     * jexxa-application.properties and the given properties object, the one from properties object is used.
+     *
+     * @param contextName Name of the BoundedContext. Typically, you should use the name of your application.
+     * @param properties Properties that are defined by your application.
+     */
     public JexxaMain(String contextName, Properties properties)
     {
         Validate.notNull(properties);
@@ -48,18 +73,31 @@ public class JexxaMain
         this.boundedContext = new BoundedContext(contextName, this);
 
         loadProperties(this.properties);
-        this.properties.putAll( properties );  //add/overwrite given properties 
+        this.properties.putAll( properties );  //add/overwrite given properties
         this.properties.put(JEXXA_CONTEXT_NAME, contextName);
 
         setExceptionHandler();
     }
 
+    /**
+     * Adds a package that is searched by Jexxa's dependency injection mechanism for creating infrastructure
+     * objects such as driven adapters.
+     * @param packageName name of the package
+     * @return JexxaMain object to call additional methods
+     */
     public JexxaMain addToInfrastructure(String packageName)
     {
         drivenAdapterFactory.whiteListPackage(packageName);
         return this;
     }
 
+    /**
+     * Adds a package that is searched by Jexxa's dependency injection mechanism for creating objects of the
+     * application core such as in- and outbound ports.
+     *
+     * @param packageName name of the package
+     * @return JexxaMain object to call additional methods
+     */
     public JexxaMain addToApplicationCore(String packageName)
     {
         portFactory.whiteListPackage(packageName);
@@ -107,7 +145,7 @@ public class JexxaMain
             LOGGER.warn("BoundedContext '{}' already started", getBoundedContext().contextName());
             return boundedContext;
         }
-        
+
         LOGGER.info("Start BoundedContext '{}' with {} Driving Adapter ", getBoundedContext().contextName(), compositeDrivingAdapter.size());
         compositeDrivingAdapter.start();
         boundedContext.start();
@@ -139,7 +177,7 @@ public class JexxaMain
     {
         return properties;
     }
-    
+
     protected void bindToPort(Class<? extends IDrivingAdapter> adapter, Class<?> port)
     {
         var drivingAdapter = drivingAdapterFactory.getInstanceOf(adapter, properties);
@@ -176,7 +214,7 @@ public class JexxaMain
 
         var portList = portFactory.getInstanceOfPorts(portAnnotation, properties);
         portList.forEach(drivingAdapter::register);
-        
+
         compositeDrivingAdapter.add(drivingAdapter);
     }
 
@@ -194,7 +232,7 @@ public class JexxaMain
                         ThrowingConsumer.exceptionLogger(properties::load),
                         () -> LOGGER.warn("NO PROPERTIES FILE FOUND {}", JEXXA_APPLICATION_PROPERTIES)
                 );
-        
+
     }
 
     private void setExceptionHandler()
@@ -217,7 +255,7 @@ public class JexxaMain
             }
             catch (RuntimeException e)
             {
-                //In case of any error we stop all driving adapter for proper cleanup and rethrow the exception 
+                //In case of any error we stop all driving adapter for proper cleanup and rethrow the exception
                 stop();
                 throw e;
             }
@@ -256,7 +294,7 @@ public class JexxaMain
         {
             this.jexxaMain = jexxaMain;
         }
-        
+
         public void uncaughtException(Thread t, Throwable e) {
             LOGGER.error("\nCould not startup Jexxa! {}", e.getMessage());
             jexxaMain.stop();
