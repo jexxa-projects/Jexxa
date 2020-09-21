3cba81a2ec2794d7080a297b54cf5c733b8cceee
repplix <michael.repplinger@icloud.com>
2020-09-21 06:12:26 +0200

    * code cleanup

--- jexxa-test/src/test/java/io/jexxa/test/JexxaTestTest.java
+++ jexxa-test/src/test/java/io/jexxa/test/JexxaTestTest.java
@@ -1,18 +1,19 @@
 package io.jexxa.test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Properties;
 
 import io.jexxa.core.JexxaMain;
 import io.jexxa.tutorials.bookstorej.domainservice.IBookRepository;
 import io.jexxa.tutorials.bookstorej.domainservice.ReferenceLibrary;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class JexxaTestTest
 {
 
     @Test
-    public void getJexxaTest()
+    public void validateRepository()
     {
         //Arrange
         JexxaMain jexxaMain = new JexxaMain(JexxaTestTest.class.getSimpleName(), new Properties());
@@ -23,7 +24,7 @@ class JexxaTestTest
         jexxaMain.bootstrap(ReferenceLibrary.class).with(ReferenceLibrary::addLatestBooks);
 
         //Assert
-        Assertions.assertTrue(bookRepository.getAll().size() > 0);
+        assertTrue(bookRepository.getAll().size() > 0);
     }
 
 }
