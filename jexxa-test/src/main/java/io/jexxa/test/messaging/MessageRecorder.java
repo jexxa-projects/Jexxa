d424c42c6261fb4f825671537664937f39aeadaf
repplix <michael.repplinger@icloud.com>
2020-09-26 13:11:58 +0200

    * added wrapper methods to MessageRecorder for RecordedMessage

--- jexxa-test/src/main/java/io/jexxa/test/messaging/MessageRecorder.java
+++ jexxa-test/src/main/java/io/jexxa/test/messaging/MessageRecorder.java
@@ -3,6 +3,8 @@ package io.jexxa.test.messaging;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.jexxa.utils.annotations.CheckReturnValue;
+
 
 public class MessageRecorder
 {
@@ -14,11 +16,13 @@ public class MessageRecorder
         recordedMessageList.add(recordedMessage);
     }
 
+    @CheckReturnValue
     public List<RecordedMessage> getMessages()
     {
         return recordedMessageList;
     }
 
+    @CheckReturnValue
     public RecordedMessage pop()
     {
         if (recordedMessageList.isEmpty())
@@ -30,4 +34,23 @@ public class MessageRecorder
         recordedMessageList.remove(0);
         return latestElement;
     }
+
+    @CheckReturnValue
+    public boolean isEmpty()
+    {
+        return recordedMessageList.isEmpty();
+    }
+
+    @CheckReturnValue
+    public int size()
+    {
+        return recordedMessageList.size();
+    }
+
+    @CheckReturnValue
+    public <T> T getMessage(Class<T> classType)
+    {
+        return classType.cast(pop());
+    }
+
 }
