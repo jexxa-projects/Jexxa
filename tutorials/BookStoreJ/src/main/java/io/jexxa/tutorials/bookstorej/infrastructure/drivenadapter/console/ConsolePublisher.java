c3ecdcc22eae46003d2f24dcd72020472358038b
repplix <michael.repplinger@icloud.com>
2020-09-26 22:38:37 +0200

    * updated ConsolePublisher of BookStoreJApplication

--- tutorials/BookStoreJ/src/main/java/io/jexxa/tutorials/bookstorej/infrastructure/drivenadapter/console/ConsolePublisher.java
+++ tutorials/BookStoreJ/src/main/java/io/jexxa/tutorials/bookstorej/infrastructure/drivenadapter/console/ConsolePublisher.java
@@ -1,28 +1,28 @@
 package io.jexxa.tutorials.bookstorej.infrastructure.drivenadapter.console;
 
-import io.jexxa.addend.applicationcore.DomainEvent;
+import java.util.Properties;
+
 import io.jexxa.addend.infrastructure.DrivenAdapter;
+import io.jexxa.infrastructure.drivenadapterstrategy.messaging.MessageSender;
+import io.jexxa.infrastructure.drivenadapterstrategy.messaging.MessageSenderManager;
 import io.jexxa.tutorials.bookstorej.domainservice.IDomainEventPublisher;
-import io.jexxa.utils.JexxaLogger;
 import org.apache.commons.lang3.Validate;
-import org.slf4j.Logger;
 
 @SuppressWarnings("unused")
 @DrivenAdapter
 public class ConsolePublisher implements IDomainEventPublisher
 {
-    private static final Logger LOGGER = JexxaLogger.getLogger(ConsolePublisher.class);
+    private final MessageSender messageSender;
+
+    public ConsolePublisher(Properties properties)
+    {
+        messageSender = MessageSenderManager.getInstance().getStrategy(properties);
+    }
 
     @Override
     public <T> void publish(T domainEvent)
     {
         Validate.notNull(domainEvent);
-
-        //Validate that we get an DomainEvent
-        Validate.notNull(domainEvent.getClass().getAnnotation(DomainEvent.class));
-
-        var logMessage = domainEvent.toString();
-
-        LOGGER.info(logMessage);
+        messageSender.send(domainEvent).toTopic("BookStoreTopic").asJson();
     }
 }
