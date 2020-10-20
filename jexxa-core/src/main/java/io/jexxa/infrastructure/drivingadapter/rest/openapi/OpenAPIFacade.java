c264db6a70ce0dc799fd78030ac3f2f65966b15e
repplix <michael.repplinger@icloud.com>
2020-10-20 18:50:27 +0200

    * first step of code cleanup: Removing hard coded stuff, removed std.out, ...

--- jexxa-core/src/main/java/io/jexxa/infrastructure/drivingadapter/rest/openapi/OpenAPIFacade.java
+++ jexxa-core/src/main/java/io/jexxa/infrastructure/drivingadapter/rest/openapi/OpenAPIFacade.java
@@ -21,8 +21,11 @@ import io.javalin.plugin.openapi.OpenApiPlugin;
 import io.javalin.plugin.openapi.annotations.HttpMethod;
 import io.javalin.plugin.openapi.dsl.DocumentedContent;
 import io.javalin.plugin.openapi.dsl.OpenApiBuilder;
-import io.swagger.v3.oas.models.examples.Example;
+import io.jexxa.utils.JexxaLogger;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.ComposedSchema;
+import io.swagger.v3.oas.models.media.IntegerSchema;
+import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
 
 @SuppressWarnings("java:S1602") // required to avoid ambiguous warnings
@@ -88,50 +91,24 @@ public class OpenAPIFacade
                 openApiDocumentation.body(method.getParameters()[0].getType());
             }  else if ( method.getParameters().length > 1 )
             {
-                try
+                var schema = new ComposedSchema();
+                var arguments = new Object[method.getParameterTypes().length];
+
+                var documentedObjects = new ArrayList<DocumentedContent>();
+
+                for (int i = 0; i < method.getParameterTypes().length; ++i)
                 {
+                    arguments[i] = createObject(method.getParameterTypes()[i]);
 
-                    var schema = new Schema<>();
-                    var arguments = new Object[method.getParameterTypes().length];
+                    documentedObjects.add(new DocumentedContent(method.getParameterTypes()[i]));
 
-                    var documentedObjects = new ArrayList<DocumentedContent>();
-                    for (int i = 0; i < method.getParameterTypes().length; ++i)
-                    {
-                        arguments[i] = createObject(createSchema(method.getParameterTypes()[i]), method.getParameterTypes()[i]);
-                        var argSchema = new Schema<>();
-
-                        if (arguments[i].getClass().equals(  Integer.class))
-                        {
-                            argSchema.setType("integer");
-                            schema.addProperties(arguments[i].getClass().getSimpleName(), argSchema);
-                        } else
-                        {
-                            argSchema.setType("object");
-                            argSchema.set$ref(arguments[i].getClass().getSimpleName());
-                            argSchema.setName(arguments[i].getClass().getSimpleName());
-                            schema.addProperties(arguments[i].getClass().getSimpleName(), argSchema);
-                            var example = new Example();
-                            //example.setValue(arguments[i]);
-                            example.set$ref(arguments[i].getClass().getSimpleName());
-                        }
-                        documentedObjects.add(new DocumentedContent(arguments[i].getClass()));
-                    }
-
-                    schema.setType("object");
-                    schema.setTitle("Array");
-                    schema.setExample(arguments);
-                    //schema.setName(arguments.getClass().getSimpleName());
-                    schema.setMaxProperties(arguments.length);
-                    schema.setMinProperties(arguments.length);
-
-                    openApiDocumentation.body(documentedObjects);
-                    openApiDocumentation.body(schema, "application/json");
-
-
-                } catch (Exception e)
-                {
-                    System.out.println(e);
+                    schema.addAnyOfItem(createSchema(method.getParameterTypes()[i]));
                 }
+
+                schema.setExample(arguments);
+
+                openApiDocumentation.body(documentedObjects);
+                openApiDocumentation.body(schema, "application/json");
             }
 
             if ( method.getReturnType() != void.class )
@@ -147,7 +124,7 @@ public class OpenAPIFacade
     }
 
 
-    private String createSchema(Class<?> clazz) throws JsonProcessingException
+    private String createSchemaAsString(Class<?> clazz) throws JsonProcessingException
     {
         var mapper = new ObjectMapper();
         //There are other configuration options you can set.  This is the one I needed.
@@ -161,30 +138,41 @@ public class OpenAPIFacade
     }
 
 
-    private Object createObject(String jsonNode, Class<?> clazz)
+    private Object createObject(Class<?> clazz)
     {
-        JsonElement element = JsonParser.parseString(jsonNode);
-        Gson gson = new Gson();
-        if (!jsonNode.contains( "\"type\":\"object\"") )
+        try
         {
-            System.out.println("here PRIMITIVE : " + clazz.getSimpleName());
-            try
+            var schemaString = createSchemaAsString(clazz);
+            JsonElement element = JsonParser.parseString(schemaString);
+            Gson gson = new Gson();
+            if (!schemaString.contains("\"type\":\"object\""))
             {
+                System.out.println("here PRIMITIVE : " + clazz.getSimpleName());
                 //TODO replace with static mapping to all primitives
                 return primitiveToWrapper(clazz).getConstructor(clazz).newInstance(0);
-            } catch (Exception e)
-            {
-                System.out.println(e);
-                return null;
             }
+            if (element.isJsonObject())
+            {
+                System.out.println("here OBJECT : " + clazz.getSimpleName());
+                return gson.fromJson(element, clazz);
+            }
+        } catch (Exception e) {
+            JexxaLogger.getLogger(getClass()).warn( "Could not create Object {}" , clazz.getName() , e );
         }
-        if (element.isJsonObject())
+        return null;
+    }
+
+    private Schema<?> createSchema(Class<?> clazz)
+    {
+        if (clazz.equals( Integer.class ) ||
+            clazz.equals( int.class ))
         {
-            System.out.println("here OBJECT : " + clazz.getSimpleName());
-            return gson.fromJson(jsonNode, clazz);
+            return new IntegerSchema();
         }
 
-        return null;
+        var result = new ObjectSchema();
+        result.set$ref(clazz.getSimpleName());
+        return result;
     }
 
 }
