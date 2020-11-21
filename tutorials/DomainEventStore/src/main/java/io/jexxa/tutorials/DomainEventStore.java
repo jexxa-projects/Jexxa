6b8985265ebcda6ae18b3706966dd902596adbd2
repplix <michael.repplinger@icloud.com>
2020-11-21 10:50:31 +0100

    * added skeleton for new tutorial

---
+++ tutorials/DomainEventStore/src/main/java/io/jexxa/tutorials/DomainEventStore.java
@@ -0,0 +1,13 @@
+package io.jexxa.tutorials;
+
+/**
+ * Hello world!
+ *
+ */
+public class DomainEventStore
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
