6a087e1f4005d92a54a9ab6efb554ce8e083b113
repplix <michael.repplinger@icloud.com>
2020-12-08 15:39:24 +0100

    * removed tutorial DomainEventStore

--- tutorials/DomainEventStore/src/main/java/io/jexxa/tutorials/domainservice/IDomainEventResend.java
+++
@@ -1,8 +0,0 @@
-package io.jexxa.tutorials.domainservice;
-
-import io.jexxa.tutorials.domain.valueobject.DomainEvent;
-
-public interface IDomainEventResend
-{
-    void resend(DomainEvent domainEvent);
-}
