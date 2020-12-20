289e13dd58f4ac194d379cd2e2add38b15f8c766
repplix <michael.repplinger@icloud.com>
2020-12-20 17:21:33 +0100

    * corrected javadoc

--- jexxa-core/src/main/java/io/jexxa/infrastructure/drivenadapterstrategy/persistence/jdbc/experimental/IRangeQuery.java
+++ jexxa-core/src/main/java/io/jexxa/infrastructure/drivenadapterstrategy/persistence/jdbc/experimental/IRangeQuery.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 public interface IRangeQuery<T, S >
 {
-    /**  get all values which fulfill:  startValue <= value
+    /**  get all values which fulfill: {@code startValue <= value}
      */
     List<T> getFrom(S startValue);
 
-    /** get all values which fulfill: startValue <= value <= endValue
+    /** get all values which fulfill: {@code startValue <= value <= endValue}
      */
     List<T> getRange(S startValue, S endValue);
 
-    /** get all values which fulfill:  value <= endValue
+    /** get all values which fulfill:  {@code value <= endValue}
      */
     List<T> getUntil(S endValue);
 }
