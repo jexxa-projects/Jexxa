ad1a3fc932f01c1bfdbb564ac44d5ce6d301837a
repplix <michael.repplinger@icloud.com>
2021-05-29 10:06:42 +0200

    added new tutorial

---
+++ tutorials/DomainEventStore/src/main/java/io/jexxa/tutorials/DomainEventStoreApplication.java
@@ -0,0 +1,13 @@
+package io.jexxa.tutorials;
+
+/**
+ * Hello world!
+ *
+ */
+public class DomainEventStoreApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
