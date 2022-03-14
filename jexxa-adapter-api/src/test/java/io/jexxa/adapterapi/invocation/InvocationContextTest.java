15f4f873b9df60c58fee11baa22fe79fd2ebf17a
Michael Repplinger <michael.repplinger@icloud.com>
2022-03-14 06:32:00 +0100

    * Code cleanup: corrected naming convention for static members

--- jexxa-adapter-api/src/test/java/io/jexxa/adapterapi/invocation/InvocationContextTest.java
+++ jexxa-adapter-api/src/test/java/io/jexxa/adapterapi/invocation/InvocationContextTest.java
@@ -17,10 +17,10 @@ class InvocationContextTest {
     private Method invocationMethod;
     private final InvocationContext[] resultingContext = new InvocationContext[4];
 
-    private static final int incrementContext = 0;
-    private static final int getCounterContext = 1;
-    private static final int setCounterContext = 2;
-    private static final int setGetCounterContext = 3;
+    private static final int INCREMENT_CONTEXT = 0;
+    private static final int GET_COUNTER_CONTEXT = 1;
+    private static final int SET_COUNTER_CONTEXT = 2;
+    private static final int SET_GET_COUNTER_CONTEXT = 3;
 
 
 
@@ -33,16 +33,16 @@ class InvocationContextTest {
 
         getRootInterceptor(objectUnderTest).registerBefore(invocationContext -> {
             if (invocationContext.getMethod().getName().equals("increment")) {
-                resultingContext[incrementContext] = invocationContext;
+                resultingContext[INCREMENT_CONTEXT] = invocationContext;
             }
             if (invocationContext.getMethod().getName().equals("setCounter")) {
-                resultingContext[setCounterContext] = invocationContext;
+                resultingContext[SET_COUNTER_CONTEXT] = invocationContext;
             }
             if (invocationContext.getMethod().getName().equals("getCounter")) {
-                resultingContext[getCounterContext] = invocationContext;
+                resultingContext[GET_COUNTER_CONTEXT] = invocationContext;
             }
             if (invocationContext.getMethod().getName().equals("setGetCounter")) {
-                resultingContext[setGetCounterContext] = invocationContext;
+                resultingContext[SET_GET_COUNTER_CONTEXT] = invocationContext;
             }
         });
     }
@@ -53,10 +53,10 @@ class InvocationContextTest {
         invokeTestMethods();
 
         //Assert
-        assertEquals("increment", resultingContext[incrementContext].getMethod().getName());
-        assertEquals("setCounter", resultingContext[setCounterContext].getMethod().getName());
-        assertEquals("getCounter", resultingContext[getCounterContext].getMethod().getName());
-        assertEquals("setGetCounter", resultingContext[setGetCounterContext].getMethod().getName());
+        assertEquals("increment", resultingContext[INCREMENT_CONTEXT].getMethod().getName());
+        assertEquals("setCounter", resultingContext[SET_COUNTER_CONTEXT].getMethod().getName());
+        assertEquals("getCounter", resultingContext[GET_COUNTER_CONTEXT].getMethod().getName());
+        assertEquals("setGetCounter", resultingContext[SET_GET_COUNTER_CONTEXT].getMethod().getName());
     }
 
     @Test
@@ -65,17 +65,17 @@ class InvocationContextTest {
         invokeTestMethods();
 
         //Assert
-        assertEquals(objectUnderTest, resultingContext[incrementContext].getTarget());
-        assertEquals(objectUnderTest, resultingContext[incrementContext].getTarget(CountingObject.class));
+        assertEquals(objectUnderTest, resultingContext[INCREMENT_CONTEXT].getTarget());
+        assertEquals(objectUnderTest, resultingContext[INCREMENT_CONTEXT].getTarget(CountingObject.class));
 
-        assertEquals(objectUnderTest, resultingContext[setCounterContext].getTarget());
-        assertEquals(objectUnderTest, resultingContext[setCounterContext].getTarget(CountingObject.class));
+        assertEquals(objectUnderTest, resultingContext[SET_COUNTER_CONTEXT].getTarget());
+        assertEquals(objectUnderTest, resultingContext[SET_COUNTER_CONTEXT].getTarget(CountingObject.class));
 
-        assertEquals(objectUnderTest, resultingContext[getCounterContext].getTarget());
-        assertEquals(objectUnderTest, resultingContext[getCounterContext].getTarget(CountingObject.class));
+        assertEquals(objectUnderTest, resultingContext[GET_COUNTER_CONTEXT].getTarget());
+        assertEquals(objectUnderTest, resultingContext[GET_COUNTER_CONTEXT].getTarget(CountingObject.class));
 
-        assertEquals(objectUnderTest, resultingContext[setGetCounterContext].getTarget());
-        assertEquals(objectUnderTest, resultingContext[setGetCounterContext].getTarget(CountingObject.class));
+        assertEquals(objectUnderTest, resultingContext[SET_GET_COUNTER_CONTEXT].getTarget());
+        assertEquals(objectUnderTest, resultingContext[SET_GET_COUNTER_CONTEXT].getTarget(CountingObject.class));
     }
 
     @Test
@@ -84,10 +84,10 @@ class InvocationContextTest {
         invokeTestMethods();
 
         //Assert
-        assertNull(resultingContext[incrementContext].getReturnValue(Integer.class));
-        assertEquals(2, resultingContext[getCounterContext].getReturnValue(Integer.class));
-        assertNull(resultingContext[setCounterContext].getReturnValue());
-        assertEquals(100, resultingContext[setGetCounterContext].getReturnValue(Integer.class));
+        assertNull(resultingContext[INCREMENT_CONTEXT].getReturnValue(Integer.class));
+        assertEquals(2, resultingContext[GET_COUNTER_CONTEXT].getReturnValue(Integer.class));
+        assertNull(resultingContext[SET_COUNTER_CONTEXT].getReturnValue());
+        assertEquals(100, resultingContext[SET_GET_COUNTER_CONTEXT].getReturnValue(Integer.class));
     }
 
     private void invokeTestMethods() throws InvocationTargetException, IllegalAccessException {
