3514fbd35d89e093033f0dc84facc2f6043c71ec
Michael Repplinger <michael.repplinger@icloud.com>
2022-06-11 09:44:41 +0200

    * [Changed] Disable Kotlin/OpenAPI warnings because it works in the context of Jexxa

--- jexxa-web/src/main/java/io/jexxa/infrastructure/drivingadapter/rest/openapi/OpenAPIConvention.java
+++ jexxa-web/src/main/java/io/jexxa/infrastructure/drivingadapter/rest/openapi/OpenAPIConvention.java
@@ -18,6 +18,7 @@ import io.javalin.plugin.openapi.annotations.HttpMethod;
 import io.javalin.plugin.openapi.dsl.DocumentedContent;
 import io.javalin.plugin.openapi.dsl.OpenApiBuilder;
 import io.javalin.plugin.openapi.dsl.OpenApiDocumentation;
+import io.javalin.plugin.openapi.utils.OpenApiVersionUtil;
 import io.jexxa.utils.JexxaLogger;
 import io.jexxa.utils.json.JSONManager;
 import io.swagger.v3.oas.models.info.Info;
@@ -66,12 +67,15 @@ public class OpenAPIConvention
     {
         this.properties = properties;
         this.javalinConfig = javalinConfig;
+
         initOpenAPI();
     }
     private void initOpenAPI()
     {
         if (properties.containsKey(JEXXA_REST_OPEN_API_PATH))
         {
+            OpenApiVersionUtil.INSTANCE.setLogWarnings(false);
+
             var applicationInfo = new Info()
                     .version(properties.getProperty(JEXXA_CONTEXT_VERSION, "1.0"))
                     .description("Auto generated OpenAPI for " + properties.getProperty(JEXXA_CONTEXT_NAME, "Unknown Context"))
