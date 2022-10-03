7e6b86b509b6f3b892c2b7e8e2aef129a0d31ab4
Michael Repplinger <michael.repplinger@icloud.com>
2022-10-03 15:00:31 +0200

    [Fix] corrected position of breakline

--- jexxa-core/src/main/java/io/jexxa/core/convention/PortConventionViolation.java
+++ jexxa-core/src/main/java/io/jexxa/core/convention/PortConventionViolation.java
@@ -14,8 +14,8 @@ public class PortConventionViolation extends RuntimeException
     PortConventionViolation(Class<?> clazz)
     {
         super("Public constructor of " + clazz.getName() + " is invalid. \n" +
-                "In case of an inbound port, all attributes of the constructor must be java-interfaces\n." +
-                "In case of a port adapter, the constructor must take a single attribute representing an inbound port\n." +
+                "In case of an inbound port, all attributes of the constructor must be java-interfaces.\n" +
+                "In case of a port adapter, the constructor must take a single attribute representing an inbound port.\n" +
                 "For more information please refer to https://jexxa-projects.github.io/Jexxa/jexxa_architecture.html#_dependency_injection_di .");
     }
 }
