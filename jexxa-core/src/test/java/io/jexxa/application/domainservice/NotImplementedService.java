273eea284aabb2e4531f7971b43f9bf698e4b647
Michael Repplinger <michael.repplinger@icloud.com>
2022-10-20 16:32:38 +0200

    [Cleanup/Docu] Code cleanup and added docu

--- jexxa-core/src/test/java/io/jexxa/application/domainservice/NotImplementedService.java
+++ jexxa-core/src/test/java/io/jexxa/application/domainservice/NotImplementedService.java
@@ -2,4 +2,5 @@ package io.jexxa.application.domainservice;
 
 public interface NotImplementedService
 {
+    //Empty interface for testing different types of valid/invalid implementations of a domain service
 }
--- jexxa-core/src/test/java/io/jexxa/application/domainservice/ValidDefaultConstructorService.java
+++ jexxa-core/src/test/java/io/jexxa/application/domainservice/ValidDefaultConstructorService.java
@@ -2,4 +2,5 @@ package io.jexxa.application.domainservice;
 
 public interface ValidDefaultConstructorService
 {
+    //Empty interface for testing different types of valid/invalid implementations of a domain service
 }
