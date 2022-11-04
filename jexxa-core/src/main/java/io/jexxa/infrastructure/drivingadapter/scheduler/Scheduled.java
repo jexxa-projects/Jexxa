0665a6783b2462a865346fbacd252f0bf69ff45a
Michael Repplinger <michael.repplinger@icloud.com>
2022-11-04 19:31:08 +0100

    [Extension #140 ] Added docu for Scheduled.java and changed default value for TimeUnit to Milliseconds

--- jexxa-core/src/main/java/io/jexxa/infrastructure/drivingadapter/scheduler/Scheduled.java
+++ jexxa-core/src/main/java/io/jexxa/infrastructure/drivingadapter/scheduler/Scheduled.java
@@ -7,14 +7,26 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Scheduled
 {
+    /**
+     * Execute the annotated method with a fixed period between invocations.
+     * <p>The time unit is milliseconds by default but can be overridden via
+     * {@link #timeUnit()}.
+     * @return the fixed period
+     */
     int fixedRate();
-    TimeUnit timeUnit() default SECONDS;
+    TimeUnit timeUnit() default MILLISECONDS;
+
+    /**
+     *  Number of units of time to delay before the first execution of a
+	 * {@link #fixedRate} task
+     * <p>The time unit is milliseconds by default but can be overridden via {@link #timeUnit()}
+     */
     int initialDelay() default 0;
 }
