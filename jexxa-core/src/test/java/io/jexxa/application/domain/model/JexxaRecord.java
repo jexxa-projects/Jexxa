a747fb7ae2ac64109d8662d250f585871b71f25d
Michael Repplinger <michael.repplinger@icloud.com>
2022-12-11 04:57:03 +0100

    [ Fix ] Corrected implementation of equals/hashCode and toString

--- jexxa-core/src/test/java/io/jexxa/application/domain/model/JexxaRecord.java
+++ jexxa-core/src/test/java/io/jexxa/application/domain/model/JexxaRecord.java
@@ -1,5 +1,27 @@
 package io.jexxa.application.domain.model;
 
+import java.util.Arrays;
+
 @SuppressWarnings("unused")
 public record JexxaRecord(String[] jexxaRecord) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JexxaRecord other = (JexxaRecord) o;
+        return Arrays.equals(this.jexxaRecord, other.jexxaRecord);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(jexxaRecord);
+    }
+
+    @Override
+    public String toString() {
+        return "JexxaRecord{" +
+                "jexxaRecord=" + Arrays.toString(jexxaRecord)+
+                '}';
+    }
 }
