c80a88a5f68f65d34b281a02e0dc09689d375723
repplix <116941865+repplix@users.noreply.github.com>
2023-01-14 15:11:23 +0100

    [ #183 ] Added test for aroundMonitor

--- jexxa-core/src/test/java/io/jexxa/core/FluentMonitorTest.java
+++ jexxa-core/src/test/java/io/jexxa/core/FluentMonitorTest.java
@@ -1,8 +1,12 @@
 package io.jexxa.core;
 
+import io.jexxa.adapterapi.invocation.InvocationContext;
+import io.jexxa.adapterapi.invocation.monitor.AroundMonitor;
 import io.jexxa.application.JexxaTestApplication;
+import io.jexxa.application.applicationservice.SimpleApplicationService;
 import io.jexxa.application.infrastructure.drivingadapter.generic.ProxyDrivingAdapter;
 import io.jexxa.application.infrastructure.drivingadapter.portadapter.ProxyPortAdapter;
+import io.jexxa.application.infrastructure.drivingadapter.generic.ProxyAdapter;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -11,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 import static io.jexxa.infrastructure.monitor.Monitors.timerMonitor;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FluentMonitorTest {
@@ -40,4 +45,52 @@ class FluentMonitorTest {
         assertFalse(boundedContext.isHealthy());
     }
 
+    @Test
+    void aroundMonitor()
+    {
+        //Arrange
+        var jexxaMain = new JexxaMain(JexxaTestApplication.class);
+        var boundedContext = jexxaMain.getBoundedContext();
+        var objectUnderTest = jexxaMain.getInstanceOfPort(SimpleApplicationService.class);
+
+        var proxyAdapter = new ProxyAdapter();
+        proxyAdapter.register(objectUnderTest);
+        jexxaMain.monitor(SimpleApplicationService.class).with(new ExceptionMonitor());
+
+        // Act
+        assertThrows(NullPointerException.class, () -> proxyAdapter.invoke(objectUnderTest::throwNullPointerException));
+
+        // Assert
+        assertFalse(boundedContext.isHealthy());
+    }
+
+    private static class ExceptionMonitor extends AroundMonitor {
+
+        private RuntimeException occurredException;
+
+        @Override
+        public boolean healthy() {
+            return occurredException == null;
+        }
+
+        @Override
+        public String getStatusMessage() {
+            if (occurredException == null)
+            {
+                return "All fine!";
+            }
+            return occurredException.getMessage();
+        }
+
+        @Override
+        public void around(InvocationContext invocationContext) {
+            try {
+                invocationContext.invoke();
+            } catch (RuntimeException e)
+            {
+                occurredException = e;
+                throw e;
+            }
+        }
+    }
 }
