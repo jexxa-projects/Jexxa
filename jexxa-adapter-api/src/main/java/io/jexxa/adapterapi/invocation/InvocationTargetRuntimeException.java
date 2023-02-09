e3a1f8254a9340b4e1108beae15e0b870e397c38
repplix <116941865+repplix@users.noreply.github.com>
2023-02-09 17:16:02 +0100

    [ Docu ] Added/Updated javadoc

--- jexxa-adapter-api/src/main/java/io/jexxa/adapterapi/invocation/InvocationTargetRuntimeException.java
+++ jexxa-adapter-api/src/main/java/io/jexxa/adapterapi/invocation/InvocationTargetRuntimeException.java
@@ -2,6 +2,11 @@ package io.jexxa.adapterapi.invocation;
 
 import java.io.Serial;
 
+/**
+ * InvocationTargetRuntimeException is an unchecked exception that wraps an exception thrown by an
+ * invoked method or constructor. This exception is preferred over InvocationTargetException in case
+ * of functional interfaces.
+ */
 public class InvocationTargetRuntimeException extends RuntimeException
 {
     @Serial
