5afdc6e03898a1c88ccd855e2652e5ba41b914c8
repplix <116941865+repplix@users.noreply.github.com>
2023-02-11 07:41:45 +0100

    [ #186 ] Implemented first version of sending messages to JMS in a separate thread. Note: Code cleanup must be done before merging to main branch

--- jexxa-core/src/main/java/io/jexxa/infrastructure/drivenadapterstrategy/outbox/TransactionalOutboxSender.java
+++ jexxa-core/src/main/java/io/jexxa/infrastructure/drivenadapterstrategy/outbox/TransactionalOutboxSender.java
@@ -1,20 +1,32 @@
 package io.jexxa.infrastructure.drivenadapterstrategy.outbox;
 
+import io.jexxa.adapterapi.invocation.InvocationManager;
+import io.jexxa.adapterapi.invocation.InvocationTargetRuntimeException;
 import io.jexxa.infrastructure.drivenadapterstrategy.messaging.MessageSender;
+import io.jexxa.infrastructure.drivenadapterstrategy.messaging.MessageSenderManager;
 import io.jexxa.infrastructure.drivenadapterstrategy.persistence.repository.IRepository;
 import io.jexxa.infrastructure.drivenadapterstrategy.persistence.repository.RepositoryManager;
+import io.jexxa.utils.JexxaLogger;
 
 import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("unused")
 public class TransactionalOutboxSender extends MessageSender {
     private final IRepository<OutboxMessage, UUID> outboxRepository;
+    private final OutboxJMSSender outboxJMSSender;
 
     public TransactionalOutboxSender(Properties properties)
     {
-        this.outboxRepository = RepositoryManager.getRepository(OutboxMessage.class
-                , OutboxMessage::messageId, properties);
+        this.outboxRepository = RepositoryManager
+                .getRepository(OutboxMessage.class
+                        , OutboxMessage::messageId
+                        , properties
+                );
+        this.outboxJMSSender = new OutboxJMSSender(properties);
     }
 
     @Override
@@ -43,6 +55,81 @@ public class TransactionalOutboxSender extends MessageSender {
                          Properties messageProperties, MessageType messageType,
                          DestinationType destinationType)
     {
+
+    }
+
+    private static class OutboxJMSSender
+    {
+        private final IRepository<OutboxMessage, UUID> outboxRepository;
+        private final Properties properties;
+        private final ScheduledExecutorService executor;
+
+        public OutboxJMSSender(Properties properties)
+        {
+            this.properties = properties;
+            this.outboxRepository = RepositoryManager
+                    .getRepository(OutboxMessage.class
+                            , OutboxMessage::messageId
+                            , properties
+                    );
+
+            this.executor = Executors.newScheduledThreadPool(1);
+            executor.scheduleAtFixedRate(() -> { System.out.println("Send messages"); transactionalSend();}, 500, 500, TimeUnit.MILLISECONDS);
+        }
+
+        @Override
+        protected void finalize() {
+            try {
+                executor.shutdown();
+                if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+                    executor.shutdownNow();
+                    System.out.println("Closed Transactional Sender");
+                }
+            } catch (InterruptedException ignored) {
+                executor.shutdownNow();
+            }
+        }
+
+        public void transactionalSend()
+        {
+            try {
+                InvocationManager.getInvocationHandler(this).invoke(this, this::sendOutboxMessages);
+            } catch (InvocationTargetRuntimeException e)
+            {
+                JexxaLogger.getLogger(getClass()).warn("Coud not send outobx messages. Reason: {}", e.getTargetException().getMessage());
+            }
+        }
+
+        public void sendOutboxMessages()
+        {
+            outboxRepository.get().stream()
+                    .filter(outboxMessage -> outboxMessage.destinationType.equals(DestinationType.QUEUE))
+                    .forEach(this::sendToQueue);
+
+            outboxRepository.get().stream()
+                    .filter(outboxMessage -> outboxMessage.destinationType.equals(DestinationType.TOPIC))
+                    .forEach(this::sendToTopic);
+
+            outboxRepository.removeAll();
+        }
+
+        @SuppressWarnings("ResultOfMethodCallIgnored")
+        void sendToQueue(OutboxMessage outboxMessage)
+        {
+            var messageSender = MessageSenderManager.getMessageSender(TransactionalOutboxSender.class, properties);
+            var producer = messageSender.send(outboxMessage.message()).toQueue(outboxMessage.destination);
+            outboxMessage.messageProperties.forEach((key, value) -> producer.addHeader((String)key, (String)value));
+            producer.asString();
+        }
+
+        @SuppressWarnings("ResultOfMethodCallIgnored")
+        void sendToTopic( OutboxMessage outboxMessage)
+        {
+            var messageSender = MessageSenderManager.getMessageSender(TransactionalOutboxSender.class, properties);
+            var producer = messageSender.send(outboxMessage.message()).toTopic(outboxMessage.destination);
+            outboxMessage.messageProperties.forEach((key, value) -> producer.addHeader((String)key, (String)value));
+            producer.asString();
+        }
 
     }
 }
